8a83205a213a659e76b89e503f276b58ff94c0e9
kalharaweragoda <kalharaweragoda@gmail.com>
2026-03-27 09:48:52 +0530

    create Route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,55 @@
-import React from 'react'
-import UserManagement from './UserManagement' 
-
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+// React Router imports එකතු කරන ලදී
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import UserManagement from './UserManagement';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RegisterModal from './components/RegisterModal';
+import ChatBox from './pages/ChatBox'; 
 import { Container, Navbar } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-    
-      <nav className="navbar navbar-dark bg-dark mb-4 p-3 shadow">
-        <div className="container">
-          <span className="navbar-brand mb-0 h1">SecureLaw Management System</span>
-        </div>
-      </nav>
-
-     
-      <main>
-        <UserManagement />
-      </main>
-
-     
-      <footer className="text-center mt-5 py-3 text-muted border-top">
-        <p>&copy; 2026 SecureLaw Gateway. All Rights Reserved.</p>
-      </footer>
-
-
   const [showModal, setShowModal] = useState(true);
 
   return (
-    <div className="main-wrapper bg-white min-vh-100">
+    <Router> 
+      <div className="App">
+        
+        <nav className="navbar navbar-dark bg-dark mb-4 p-3 shadow">
+          <div className="container">
+            <span className="navbar-brand mb-0 h1">
+              SecureLaw Management System
+            </span>
+          </div>
+        </nav>
 
-      <Navbar expand="lg" className="navbar-custom py-3 px-4">
-        <Container>
-          <Navbar.Brand href="#" className="fw-bold navbar-brand-glow">
+        <Navbar expand="lg" className="navbar-custom py-3 px-4">
+          <Container>
+            <Navbar.Brand href="#" className="fw-bold navbar-brand-glow">
+            </Navbar.Brand>
+          </Container>
+        </Navbar>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<UserManagement />} />
             
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
+            <Route path="/chat" element={<ChatBox />} />
+          </Routes>
+        </main>
 
-      <RegisterModal
-        show={showModal}
-        handleClose={() => setShowModal(false)}
-      />
+        <RegisterModal
+          show={showModal}
+          handleClose={() => setShowModal(false)}
+        />
 
-    </div>
-  )
+        <footer className="text-center mt-5 py-3 text-muted border-top">
+          <p>&copy; 2026 SecureLaw Gateway. All Rights Reserved.</p>
+        </footer>
+
+      </div>
+    </Router>
+  );
 }
 
-
-export default App
-
 export default App;
-
